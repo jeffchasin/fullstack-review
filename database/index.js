4d51f1374e5997d7a0830452756f0415a2d295e4
Jeff Chasin <jeff@jeffchasin.com>
2018-09-21 19:51:01 -0400

    add diagram and /database/index schema

--- database/index.js
+++ database/index.js
@@ -1,16 +1,42 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+/*
+CREATE TABLE repos (
+    id INT,
+    name VARCHAR(255) NOT NULL,
+    owner_login VARCHAR(24) NOT NULL,
+    html_url TEXT NOT NULL,
+    stargazers_count INT,
+    PRIMARY KEY (id)
+);
+*/
+
+let db = mongoose.connection;
+db.on('error', console.error('DB connection error: ', error));
+
+db.once('open', function () {
+  let repoSchema = mongoose.Schema({
+    // TODO your schema here!
+    id: {
+      type: Number,
+      required: true,
+      index: true,
+      unique: true
+    },
+    name: { type: String, required: true, },
+    owner_login: { type: String, required: true },
+    html_url: { type: String, required: true },
+    stargazers_count: { type: Number }
+  });
+
+  let Repo = mongoose.model('Repo', repoSchema);
+
+  let save = (/* TODO */) => {
+    // TODO: Your code here
+    // This function should save a repo or repos to
+    // the MongoDB
+  }
 });
-
-let Repo = mongoose.model('Repo', repoSchema);
-
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
-}
 
 module.exports.save = save;
